a5e2b55c27d1128bc1a060dffe17b73347d11b2d
EadanL <eadan.lay@ucalgary.ca>
2023-03-08 18:00:02 -0700

    moved blank.jsx content into route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
+import Notes from "./components/notes";
+
 import Layout from "./components/layout";
 import Editor from "./components/editor";
-import Blank from "./components/blank";
 import Viewer from "./components/viewer";
 
 function App() {
-	let { pageId } = useParams();
+	// const [notes, setNotes] = useState([]);
+
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route element={<Layout />}>
 					{/* <Route path="/edit" element={<Viewer />}></Route> */}
-					<Route path="/" element={<Blank />}></Route>
+					<Route
+						path="/"
+						element={
+							<div id="note-container">
+								<p id="empty-notes">
+									Select a note, or create a new one.
+								</p>
+							</div>
+						}
+					></Route>
 					<Route path="/edit" element={<Editor />}></Route>
 				</Route>
 			</Routes>
--- src/components/blank.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Blank() {
-	return (
-		<div id="note-container">
-			<p id="empty-notes">Select a note, or create a new one.</p>
-		</div>
-	);
-}
